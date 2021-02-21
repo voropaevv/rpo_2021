2ec25533d1c766d8c1fc3994af778b2245b17bb2
Voropaev Vlad <vladislavvoropaev2001@gmail.com>
2021-02-21 09:31:29 +0300

    add encrypt and decrypt

--- fclient/app/src/main/cpp/native-lib.cpp
+++ fclient/app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,7 @@
 #include "spdlog/sinks/android_sink.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
+#include "mbedtls/des.h"
 
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "fclient_ndk", __VA_ARGS__)
 #define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
@@ -17,7 +18,7 @@ char *personalization = "fclient-sample-app";
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_ru_iu3_fclient_MainActivity_stringFromJNI(JNIEnv* env, jobject) {
-    std::string hello = "My first project. Random number: ";
+    std::string hello = "My first project.";
     LOG_INFO("Hello from system log %d", 2021);
     SLOG_INFO("Hello from spdlog {}", 2021);
     return env->NewStringUTF(hello.c_str());
@@ -45,4 +46,75 @@ Java_ru_iu3_fclient_MainActivity_randomBytes(JNIEnv* env, jclass, jint no) {
     env->SetByteArrayRegion(rnd, 0, no, (jbyte *)buf);
     delete[] buf;
     return rnd;
+}
+
+// Енкрипт
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_ru_iu3_fclient_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
+{
+
+    jsize ksz = env->GetArrayLength(key);
+    jsize dsz = env->GetArrayLength(data);
+    if ((ksz != 16) || (dsz <= 0)) {
+        return env->NewByteArray(0);
+    }
+
+    mbedtls_des3_context ctx;
+    mbedtls_des3_init(&ctx);
+
+    jbyte * pkey = env->GetByteArrayElements(key, 0);
+
+    // PKCS#5 padding
+    int rst = dsz % 8;
+    int sz = dsz + 8 - rst;
+    uint8_t * buf = new uint8_t[sz];
+    for (int i = 7; i > rst; i--)
+        buf[dsz + i] = rst;
+    jbyte * pdata = env->GetByteArrayElements(data, 0);
+    std::copy(pdata, pdata + dsz, buf);
+    mbedtls_des3_set2key_enc(&ctx, (uint8_t *)pkey);
+    int cn = sz / 8;
+    for (int i = 0; i < cn; i++)
+        mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf + i*8);
+    jbyteArray dout = env->NewByteArray(sz);
+    env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
+    delete[] buf;
+    env->ReleaseByteArrayElements(key, pkey, 0);
+    env->ReleaseByteArrayElements(data, pdata, 0);
+    return dout;
+}
+
+// Декрипт
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_ru_iu3_fclient_MainActivity_decrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
+{
+    jsize ksz = env->GetArrayLength(key);
+    jsize dsz = env->GetArrayLength(data);
+    if ((ksz != 16) || (dsz <= 0) || ((dsz % 8) != 0)) {
+        return env->NewByteArray(0);
+    }
+
+    mbedtls_des3_context ctx;
+    mbedtls_des3_init(&ctx);
+
+    jbyte * pkey = env->GetByteArrayElements(key, 0);
+
+    uint8_t * buf = new uint8_t[dsz];
+
+    jbyte * pdata = env->GetByteArrayElements(data, 0);
+    std::copy(pdata, pdata + dsz, buf);
+    mbedtls_des3_set2key_dec(&ctx, (uint8_t *)pkey);
+    int cn = dsz / 8;
+    for (int i = 0; i < cn; i++)
+        mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf +i*8);
+
+    //PKCS#5. simplified. need to check every padded byte for security reasons
+    int sz = dsz - 8 + buf[dsz-1];
+
+    jbyteArray dout = env->NewByteArray(sz);
+    env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
+    delete[] buf;
+    env->ReleaseByteArrayElements(key, pkey, 0);
+    env->ReleaseByteArrayElements(data, pdata, 0);
+    return dout;
 }
--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -19,10 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        byte[] rnd = randomBytes(16);
+        byte[] key = randomBytes(16);
+        byte[] data = "Text__Encryption".getBytes();
+        byte[] EncryptData = encrypt(key, data);
+        byte[] DecryptData = decrypt(key, EncryptData);
+        String EncryptText = new String(EncryptData, StandardCharsets.UTF_8);
+        String DecryptText = new String(DecryptData, StandardCharsets.UTF_8);
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI() + rnd[0]);
+        tv.setText(stringFromJNI() + '\n' + "EncryptText: " + EncryptText + '\n'
+                   + "DecryptText: " + DecryptText);
     }
 
     /**
@@ -32,4 +40,6 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
